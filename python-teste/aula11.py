8b371b2f3009233ddbcbc00e05b43488ca1591a6
Gabriel Ribeiro <gabrielribeirosoares@hotmail.com>
2023-07-31 15:58:21 -0300

    fim do módulo

---
+++ python-teste/aula11.py
@@ -0,0 +1,3 @@
+nome = 'Gabriel'
+cores = {'limpa':'\33[m', 'azul':'\33[34m', 'amarelo':'\33[33m','pretoebranco':'\33[7;30m'}
+print('Olá! Muito prazer em te conhecer, {}{}{}'.format(cores['amarelo'], nome, cores['limpa']))
