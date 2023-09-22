87638008c8fc211e45de9b734fe69dc29407caa7
Gabriel Ribeiro <gabrielribeirosoares@hotmail.com>
2023-09-22 19:15:47 -0300

    att desafios

---
+++ desafios/d054.py
@@ -0,0 +1,14 @@
+from datetime import date
+atual = date.today().year
+totalmaior = 0
+totalmenor = 0
+for pess in range(1, 8):
+    nasc = int(input('Em que ano a {}ª pessoa nasceu?'.format(pess)))
+    idade = atual - nasc
+    if idade >= 21:
+        totalmaior += 1
+    else:
+        totalmenor += 1
+print('Ao todo tivemos {} pessoas maiores de idade'.format(totalmaior))
+print('E tambem tivemos {} pessoas menores de idade'.format(totalmenor))
+
